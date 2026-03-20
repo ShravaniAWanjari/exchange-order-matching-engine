a8be11088da0ac42defb31b29c1bd5467349189e
ShravaniAWanjari <shravani22wanjari@gmail.com>
2026-03-20 19:15:32 +0530

    order book header file code

---
+++ src/order_book.hpp
@@ -0,0 +1,30 @@
+#ifndef ORDER_BOOK_HPP
+#define ORDER_BOOK_HPP
+
+#include <cstdint>
+#include <deque>
+#include <functional>
+#include <map>
+
+#include "order.hpp"
+
+class OrderBook{
+    public:
+    using OrderQueue = std::deque<Order>;
+    using AskLevels = std::map<double, OrderQuantity>;
+    using BidLevels = std::map<double, OrderQueue, std::greater<double>>;
+
+    OrderBook();
+
+    void addOrder(const OrderRequest& request);
+    void printBook() const;
+
+private:
+    BidLevels bids_;
+    AskLevels asks_;
+    std::uint64_t nextSequence_;
+
+    Order makeOrder(const OrderRequest& request);
+};
+
+#endif
