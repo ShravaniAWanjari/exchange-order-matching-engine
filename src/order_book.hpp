8fa73f1439bc64a4c52763bcbaa73bede99745ca
ShravaniAWanjari <shravani22wanjari@gmail.com>
2026-03-22 12:21:24 +0530

    replaced orderqueue by pricelevel, volume aggregation, added cancellation

--- src/order_book.hpp
+++ src/order_book.hpp
@@ -5,23 +5,37 @@
 #include <deque>
 #include <functional>
 #include <map>
+#include <unordered_map>
 
 #include "order.hpp"
 
+struct PriceLevel{
+    std::deque<Order> orders;
+    std::uint32_t totalQuantity;
+
+    PriceLevel() : totalQuantity(0){};
+};
+
+struct OrderLocator {
+    Side side;
+    double price;
+}
+
 class OrderBook{
     public:
-    using OrderQueue = std::deque<Order>;
-    using AskLevels = std::map<double, OrderQueue>;
-    using BidLevels = std::map<double, OrderQueue, std::greater<double>>;
+    using AskLevels = std::map<double, PriceLevel>;
+    using BidLevels = std::map<double, PriceLevel, std::greater<double>>;
 
     OrderBook();
 
     void addOrder(const OrderRequest& request);
+    bool cancelOrder(std::uint64_t orderId)
     void printBook() const;
 
 private:
     BidLevels bids_;
     AskLevels asks_;
+    std::unordered_map<std::uint64_t, OrderLocator> orderIndex_;
     std::uint64_t nextSequence_;
 
     Order makeOrder(const OrderRequest& request);
