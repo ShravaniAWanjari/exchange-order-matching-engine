d97bede9b8a74b408b75c3d16d20278b5870888d
ShravaniAWanjari <shravani22wanjari@gmail.com>
2026-03-22 11:38:31 +0530

    making some changes to main.cpp to test if the priority logic works

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 int main(){
     OrderBook book;
 
-    book.addOrder(OrderRequest{1,100.0,50, Side::Sell});
-    book.addOrder(OrderRequest{2,99.0,40,Side::Sell});
-    book.addOrder(OrderRequest{3,101.0,60,Side::Buy});
+    book.addOrder(OrderRequest{1,100.0,20, Side::Buy});
+    book.addOrder(OrderRequest{2,100.0,40,Side::Buy});
+    book.addOrder(OrderRequest{3,90.0,10,Side::Sell});
 
     book.printBook();
 
